ed800f2f8c2401983bc831c023ee50312b8e387c
asif riad <riad15-10969@diu.edu.bd>
2021-06-04 14:07:55 +0600

    Add files via upload

---
+++ uri8.py
@@ -0,0 +1,6 @@
+employee = int(input())
+work_hour = int(input())
+amount_per_hour = float(input())
+SALARY = work_hour*amount_per_hour
+print("NUMBER = %i"%employee)
+print("SALARY = U$ %0.2f"%SALARY)

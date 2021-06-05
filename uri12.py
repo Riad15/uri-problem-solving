d68e2aa1378385663800aa634c6e4016bf025f61
asif riad <riad15-10969@diu.edu.bd>
2021-06-05 13:33:36 +0600

    Add files via upload

---
+++ uri12.py
@@ -0,0 +1,16 @@
+A, B, C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+
+
+TRIANGULO = .5*A*C
+CIRCULO = 3.14159*C*C
+TRAPEZIO = (A+B)/2*C
+QUADRADO = B*B
+RETANGULO = A*B
+print("TRIANGULO: %0.3f"%TRIANGULO)
+print("CIRCULO: %0.3f"%CIRCULO)
+print("TRAPEZIO: %0.3f"%TRAPEZIO)
+print("QUADRADO: %0.3f"%QUADRADO)
+print("RETANGULO: %0.3f"%RETANGULO)

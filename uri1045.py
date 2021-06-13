67f4f59c8d99c2a79dd4cbee8382f95f1d633c3a
asif riad <riad15-10969@diu.edu.bd>
2021-06-14 01:06:31 +0600

    Add files via upload

---
+++ uri1045.py
@@ -0,0 +1,27 @@
+a, b, c =map(float,input().split())
+if a<b:
+    temp = a
+    a=b
+    b=temp
+if b<c:
+    temp = b
+    b=c
+    c =temp
+if a<b:
+    temp = a
+    a=b
+    b=temp
+if a>=(b+c):
+    print("NAO FORMA TRIANGULO")
+elif ((a*a) == (b*b + c*c)):
+    print("TRIANGULO RETANGULO")
+elif ((a*a)>(b*b+c*c)):
+    print("TRIANGULO OBTUSANGULO")
+elif ((a*a)< (b*b+c*c)):
+    print("TRIANGULO ACUTANGULO")
+elif a==b and b==c:
+    print("TRIANGULO EQUILATERO")
+elif a==b or b==c:
+    print("TRIANGULO ISOSCELES")
+
+
